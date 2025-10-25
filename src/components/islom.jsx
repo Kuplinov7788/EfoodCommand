12a15a2165e99dc5e52d20d85531153ae1728f75
islom709 <rislom709@gmail.com>
2025-10-25 15:01:33 +0500

    chala

---
+++ src/components/islom.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const islom = () => {
+  return (
+    <div>
+        <div className="main">
+            <div className="wrapper">
+                <h1>
+                    About us
+                </h1>
+                <p>In just a couple of years, we have opened 6 outlets in different cities: Kazan, Chelyabinsk, Ufa, Samara, Izhevsk, and in the future we plan to develop the network in other major cities of Russia.</p>
+                <img src="" alt="" />
+                <p>The kitchen of each point is at least: 400-500 sq. m. meters, hundreds of employees, smoothly performing work in order to receive / prepare / form / deliver customer orders on time.</p>
+            </div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default islom
