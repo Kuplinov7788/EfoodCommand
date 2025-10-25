e9d959d40bf4eb4bc73662f841f75cee9c35a42c
Muhammadrizo <ilhomovmuhammadrizo141@gmail.com>
2025-10-25 16:28:25 +0500

    asd

--- src/Components/Rizo.jsx
+++ src/Components/Rizo.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import '../Components/Rizo.css'
-
+import font from '../fonts/mulish-v18-latin-300.woff2'
+import font2 from '../fonts/mulish-v18-latin-regular.woff2'
+import img from '../assets/Rectangle 11.png'
 
 const Rizo = () => {
   return (
     <div>
-        <h1>jasur </h1>
-
+       <header>
+        <div className="container">
+            <div className="header__wrapper">
+                <div className="header__card1">
+                    <h1 className='header__h1'>HOW WE COOKING</h1>
+                    <button className='header__btn'>More</button>
+                </div>
+                <div className="header__card2">
+                    
+                </div>
+                <div className="header__text">
+                    <h1></h1>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+       </header>
     </div>
   )
 }
